18b0343df7bc3040e0cf530392789229e936a14c
Nijat Majidov <97800703+nicat827@users.noreply.github.com>
2025-03-30 23:38:21 +0400

    166. Majority Element

---
+++ LeetCodeSolutions.MajorityElement169/Program.cs
@@ -0,0 +1,21 @@
+﻿int MajorityElement(int[] nums)
+{
+    int majority = nums[0];
+    int count = 1;
+    for (int i = 1; i < nums.Length; i++)
+    {
+        if (count == 0)
+            majority = nums[i];
+
+        if (nums[i] == majority)
+            count++;
+        else
+            count--;
+    }
+    return majority;
+}
+
+int[] input = new int[] { 1 };
+int output = MajorityElement(input);
+
+Console.WriteLine(output);
