3401c9bb3713c0aa4c1e14ed9d8d0406daf21ce7
Nijat Majidov <97800703+nicat827@users.noreply.github.com>
2025-03-31 00:58:32 +0400

    27. Remove Element

---
+++ LeetCodeSolutions.RemoveElement27/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+
+int RemoveElement(int[] nums, int val)
+{
+    int idx = 0;
+    for (int i = 0; i < nums.Length; i++)
+    {
+        if (nums[i] != val)
+        {
+            nums[idx] = nums[i];
+            idx++;
+        }
+    }
+    for (int i = 0; i < nums.Length; i++)
+    {
+        Console.WriteLine(nums[i]);
+    }
+    return idx;
+}
+
+int[] input = new int[] { 1,2,2,4,5,2,3,2 };
+int output = RemoveElement(input, 2);
+
+
+
+//Console.WriteLine(output);
