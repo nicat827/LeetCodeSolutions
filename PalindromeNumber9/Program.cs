28664c11b0433dfc4a091dab4c31a83d67128f94
Nicat Macidov <nicat.macidov@azintelecom.az>
2025-04-02 17:45:03 +0400

    7.  Palindrome Number

---
+++ PalindromeNumber9/Program.cs
@@ -0,0 +1,17 @@
+﻿bool IsPalindrome(int x)
+{
+    int original = x;
+    
+    int reversed = 0;
+    while (x != 0)
+    {
+        int remainder = x % 10;
+        reversed = reversed * 10 + remainder;
+        x /= 10;
+    }
+    return reversed == original;
+
+}
+
+bool res = IsPalindrome(-121);
+Console.WriteLine(res);
