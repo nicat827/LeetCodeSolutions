517f1d69223a34faa335e0df5432ed666684dcad
Nicat Macidov <nicat.macidov@azintelecom.az>
2025-04-02 18:00:04 +0400

    init: ReverseInt7

---
+++ LeetCodeSolutions.ReverseInteger7/Program.cs
@@ -0,0 +1,14 @@
+﻿int Reverse(int x)
+{
+    if (x >= int.MaxValue || x <= int.MinValue) return 0;
+    int reversed = 0;
+    while (x != 0)
+    {
+        reversed = reversed * 10 + x % 10;
+        x /= 10;
+    }
+    if (reversed >= int.MaxValue || reversed <= int.MinValue) return 0;
+    return reversed;
+}
+int res = Reverse(-123);
+Console.WriteLine(res);
