b463956a4653f99f3f4a12fba2021525ed1295ba
Nicat Macidov <nicat.macidov@azintelecom.az>
2025-04-03 14:45:36 +0400

    58. Length Of Last Word

---
+++ LeetCodeSolutions.LengthOfLastWord58/Program.cs
@@ -0,0 +1,16 @@
+﻿int LengthOfLastWord(string s)
+{
+    int count = 0;
+    for (int i = s.Length - 1; i >= 0; i--)
+    {
+        if (s[i] != ' ')
+        {
+            count++;
+        }
+        else if  (count > 0) return count;
+    }
+    return count;
+}
+
+int length = LengthOfLastWord("hello this is test  ");
+Console.WriteLine(length);
